7fe3617865ac0fe051edcdda8b5e13cc35e7da1e
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-07-17 13:59:39 +0530

    added packages

--- src/main/java/com/example/demo/DummyController.java
+++ src/main/java/com/example/demo/DummyController.java
@@ -3,15 +3,22 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @RestController
 public class DummyController {
   
-  @RequestMapping(value = "/", method = RequestMethod.GET)
+  @RequestMapping(value = "/hi", method = RequestMethod.GET)
   String displayDefaultMessage() {
     return "Hello Worcvcvcld!";
+  }
+  
+  @RequestMapping(value = "/hio", method=RequestMethod.GET)
+  public String requestMethodName(@RequestParam String param) {
+      return "Hello thid id in HI page";
   }
   
 
